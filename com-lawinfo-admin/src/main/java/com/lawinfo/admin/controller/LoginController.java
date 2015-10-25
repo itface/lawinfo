0c6b4f1680831c47e0da9ec691a0ef6b24b8108a
wangrongtao <wangrongtao@jd.com>
2015-10-25 20:21:45 +0800

    adfadd

--- com-lawinfo-admin/src/main/java/com/lawinfo/admin/controller/LoginController.java
+++ com-lawinfo-admin/src/main/java/com/lawinfo/admin/controller/LoginController.java
@@ -29,18 +29,44 @@ public class LoginController {
     }
 
     @RequestMapping("/dologin")
+    @ResponseBody
     public String dologin(HttpServletRequest request,String tel,String code) {
         LoginResultEnum loginResultEnum = loginService.login(tel, code);
+        StringBuilder sb = new StringBuilder();
         if (("admin".equals(tel) && "admin123".equals(code))||(loginResultEnum!=null&&loginResultEnum.getCode()==LoginResultEnum.SUCCESS.getCode())) {
             LoginInfo.addUseridToSession(request.getSession(), tel);
-            return "redirect:/admin/index";
+            if ("admin".equals(tel)) {
+                sb.append("{");
+                sb.append("\"code\":").append(0).append(",");
+                sb.append("\"desc\":\"").append("\",");
+                sb.append("\"type\":").append(1).append("");
+                sb.append("}");
+            }else{
+                sb.append("{");
+                sb.append("\"code\":").append(0).append(",");
+                sb.append("\"desc\":\"").append("\",");
+                sb.append("\"type\":").append(0).append("");
+                sb.append("}");
+            }
+//            return "redirect:/admin/index";
+        }else{
+            sb.append("{");
+            sb.append("\"code\":").append(loginResultEnum.getCode()).append(",");
+            sb.append("\"desc\":\"").append(loginResultEnum.getDesc()).append("\",");
+            sb.append("\"type\":").append(0).append("");
+            sb.append("}");
         }
-        return "redirect:/login/page";
+        return sb.toString();
     }
     @RequestMapping("/sendsms")
     @ResponseBody
     public String sendsms(String tel) {
         EnSendSmsResultEnum enSendSmsResultEnum = enSendSmsService.sendSms(tel);
-        return enSendSmsResultEnum.getCode()+","+enSendSmsResultEnum.getDesc();
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        sb.append("\"code\":").append(enSendSmsResultEnum.getCode()).append(",");
+        sb.append("\"desc\":").append(enSendSmsResultEnum.getDesc());
+        sb.append("}");
+        return sb.toString();
     }
 }
