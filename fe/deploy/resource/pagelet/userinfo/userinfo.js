2d296f230692fff0801e17ef1ee3131a4c939283
lujingfeng <lujingfeng@haodehaode.cn>
2015-10-25 19:40:53 +0800

    add

---
+++ fe/deploy/resource/pagelet/userinfo/userinfo.js
@@ -0,0 +1,25 @@
+define('pagelet/userinfo/userinfo.js', function(require, exports, module){ var userinfo = {
+    init: function(){
+        $("#record-nav").on("click", "li",function(e){
+            e.preventDefault();
+            $(this).parent().find(".active").removeClass("active");
+            $(this).addClass("active");
+        });
+
+        $("#btn-statistic").on("click", function(){
+            $("#statistic").show();
+        });
+
+        $(".list tbody").on("click", "tr", function(){
+            $("#progress").modal({ keyboard: true }).modal("show");
+        });
+
+
+        $("#btn-case-add").on("click", function(){
+            $("#case-add").modal({ keyboard: true }).modal("show");
+        });
+    }
+};
+
+module.exports = userinfo; 
+});
