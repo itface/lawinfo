f1a054e6232bcddc060dfa6f712ed52e830cccf3
wangrongtao <wangrongtao@jd.com>
2015-10-25 19:42:37 +0800

    aa

---
+++ com-lawinfo-admin/src/main/java/com/lawinfo/admin/controller/CaseProgressController.java
@@ -0,0 +1,27 @@
+package com.lawinfo.admin.controller;
+
+import com.lawinfo.admin.system.login.LoginInfo;
+import com.lawinfo.service.login.LoginService;
+import com.lawinfo.service.login.enumtype.LoginResultEnum;
+import com.lawinfo.service.sms.EnSendSmsService;
+import com.lawinfo.service.sms.enumtype.EnSendSmsResultEnum;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by wangrongtao on 15/10/18.
+ */
+@Controller
+@RequestMapping("/lawinfo/caseprogress")
+public class CaseProgressController {
+
+
+    @RequestMapping("/add")
+    public String index() {
+        return "/admin/login";
+    }
+}
