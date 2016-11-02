f1778962f0a3f69a7c68d5ac45c1c243631338d3
wangrongtao <wangrongtao@jd.com>
2016-11-02 20:28:29 +0800

    wechat

--- com-lawinfo-service/src/main/java/com/lawinfo/service/util/WeChatInfo.java
+++ com-lawinfo-service/src/main/java/com/lawinfo/service/util/WeChatInfo.java
@@ -4,7 +4,7 @@ package com.lawinfo.service.util;
  * Created by wangrongtao on 2016/10/26.
  */
 public class WeChatInfo {
-    public final static String token="";
-    public final static String appId="";
-    public final static String appSecret="";
+    public final static String token="111";
+    public final static String appId="222";
+    public final static String appSecret="333";
 }
